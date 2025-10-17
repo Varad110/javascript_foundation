591362a8308d6c8da0f081e1815c2ea359cc0ec0
Varad Gujarathi <varadgujarathi120@gmail.com>
2025-10-17 21:15:45 +0530

    created a datatype.js to learn about the datatypes in js

---
+++ part1/datatype.js
@@ -0,0 +1,32 @@
+console.log("hello");
+// console.log("chai");
+/*
+
+string
+number
+boolean
+bigint
+
+undefined
+null
+
+object
+
+symbol
+
+*/
+
+// var score = 102;
+
+let score = 102;
+let name = "chaicode.com";
+let isaloggedin = false;
+
+// object
+
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = { firstname: "varad", lastname: "gujarathi" };
+
+let getScore = score;
+
+console.log(getScore);
