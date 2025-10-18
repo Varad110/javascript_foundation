1805f12fcb258d0c4c996846d1e5e644eee91756
Varad Gujarathi <varadgujarathi120@gmail.com>
2025-10-18 23:06:04 +0530

    created operation, logical, and opepre files

---
+++ part1/logical.js
@@ -0,0 +1,13 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedin = true;
+let isPaid = true;
+
+console.log(isLoggedin && isPaid);
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
---
+++ part1/operations.js
@@ -0,0 +1,30 @@
+let score = 102;
+let bouns = 25;
+
+let totleScore = score + bouns;
+
+let addition = 4 + 5;
+let substraction = 9 - 3;
+let multi = 3 * 5;
+let divi = 8 / 2;
+let reminder = 9 % 2;
+let expo = 2 ** 3;
+
+let myscore = 110;
+myscore++;
+++myscore;
+
+let credits = 55;
+credits--;
+--credits;
+
+//comparison operators
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+console.log(num1 == num2);
+console.log(num1 == num3);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
