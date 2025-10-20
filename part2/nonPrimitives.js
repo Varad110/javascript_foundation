ff7aa8d3ecdf21e70b5c3f2a356c518c78340735
Varad Gujarathi <varadgujarathi120@gmail.com>
2025-10-20 21:10:11 +0530

    created new file for nonPrimitive datatypes

---
+++ part2/nonPrimitives.js
@@ -0,0 +1,27 @@
+const username = {
+  "first name": "Mr. V",
+  isLoggedin: true,
+};
+
+username["first name"] = "Mr. Varad";
+username.lastname = "Gujarathi";
+
+// console.log(username["first name"]);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array
+let heros = ["a", "b", "c"];
+let anotherUser = ["varad", true];
+
+//console.log(anotherUser[0]);
+
+console.log("1" + 1);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
